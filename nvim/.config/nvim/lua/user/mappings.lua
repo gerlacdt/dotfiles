4ea6593efee7aff12a55fcd316fd6afd6171a9bd
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2023-05-26 10:51:19 +0200

    added hop.nvim

--- nvim/.config/nvim/lua/user/mappings.lua
+++ nvim/.config/nvim/lua/user/mappings.lua
@@ -11,7 +11,10 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(
+            bufnr)
+        end)
       end,
       desc = "Pick to close",
     },
@@ -20,6 +23,7 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>v"] = { "<cmd>HopWord<cr>", desc = "HopWord" },
   },
   t = {
     -- setting a mapping to false will disable it
