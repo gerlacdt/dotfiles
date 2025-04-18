a76639e325b22eb301faa2ac830e8c42bd86018b
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2025-04-18 20:29:06 +0200

    add spell checking shortcut for neovim

--- nvim/.config/nvim/lua/plugins/astrocore.lua
+++ nvim/.config/nvim/lua/plugins/astrocore.lua
@@ -72,7 +72,8 @@ return {
           end,
           desc = "Close buffer from tabline",
         },
-
+        ["<Leader>bt"] = { "<cmd>setlocal spell<cr>", desc = "enable spell checking",
+        },
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         -- ["<Leader>b"] = { desc = "Buffers" },
