67c8bc64e72138d49345054c46d7d85ac12a1b54
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2025-04-28 19:44:56 +0200

    enable prettier for markdown mode in emacs

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -69,6 +69,10 @@
 ;;
 (map! :map evil-window-map
       "o" #'delete-other-windows)
+
+(after! apheleia
+  (setf (alist-get 'markdown-mode apheleia-mode-alist)
+        '(prettier-markdown)))
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
